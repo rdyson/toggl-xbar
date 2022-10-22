952ecc4e34fcb69fd762bd24f38d4924448d48d2
Rob Dyson <rdyson@hotmail.com>
2022-10-22 15:24:01 +0100

    Moved workspace ids to an array and loop over them

--- toggl.10m.sh
+++ toggl.10m.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
 export PATH='/bin:/usr/local/bin:/usr/bin:/opt/homebrew/bin:$PATH'
-#  <xbar.title>Toggle Bar</xbar.title>
+#  <xbar.title>Toggl Xbar</xbar.title>
 #  <xbar.version>v1.0</xbar.version>
 #  <xbar.author>Rob Dyson</xbar.author>
 #  <xbar.author.github>rdyson</xbar.author.github>
-#  <xbar.desc>Displays current week's total time logged in Toggl</xbar.desc>
+#  <xbar.desc>Displays current week's total time logged in Toggl for one or more workspaces</xbar.desc>
 
-workspace_one=2838545
-workspace_two=6790797
+# Separate multiple workspaces with a space
+workspaces=(2838545 6790797)
 
+# Specifies previous Monday, can be changed to Sunday if you prefer
 start_date=$(date -v Monday +"%Y"-"%m"-"%d")
 
-one=$(curl -s -X POST https://api.track.toggl.com/reports/api/v3/workspace/"$workspace_one"/projects/summary \
+# Loop over all workspaces in workspaces array, summing total seconds.
+# Curl uses login credentials stored in ~/.netrc, see repo for an example.
+for workspace in ${workspaces[@]}
+do
+ workspace_seconds=$(curl -s -X POST https://api.track.toggl.com/reports/api/v3/workspace/"$workspace"/projects/summary \
   -H "Content-Type: application/json" \
   -d '{"start_date":"'"$start_date"'"}' \
   -n | jq -c '[.[].tracked_seconds'] | sed 's/\[//' | sed 's/\]//' | sed 's/,/+/g')
+   total_seconds=$(($total_seconds+$workspace_seconds))
+done
 
-two=$(curl -s -X POST https://api.track.toggl.com/reports/api/v3/workspace/"$workspace_two"/projects/summary \
-  -H "Content-Type: application/json" \
-  -d '{"start_date":"'"$start_date"'"}' \
-  -n | jq -c '[.[].tracked_seconds'] | sed 's/\[//' | sed 's/\]//' | sed 's/,/+/g')
-
-echo "$((($one+$two)/3600))h";
+# Print total seconds as hours.
+echo "$((($total_seconds)/3600))h";
