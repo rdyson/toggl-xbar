08b93fe13607a690415b80303932da3bb4031c66
Rob Dyson <rdyson@hotmail.com>
2022-10-24 07:47:22 +0100

    Handle case when tracked_seconds is empty

--- toggl.10m.sh
+++ toggl.10m.sh
@@ -14,12 +14,22 @@ start_date=$(date -v -Monday +"%Y"-"%m"-"%d")
 
 # Loop over all workspaces in workspaces array, summing total seconds.
 # Curl uses login credentials stored in ~/.netrc, see repo for an example.
+# Get the "tracked_seconds" value from the curl response and remov brackets and padding.
+
 for workspace in ${workspaces[@]}
 do
  workspace_seconds=$(curl -s -X POST https://api.track.toggl.com/reports/api/v3/workspace/"$workspace"/projects/summary \
   -H "Content-Type: application/json" \
   -d '{"start_date":"'"$start_date"'"}' \
   -n | jq -c '[.[].tracked_seconds'] | sed 's/\[//' | sed 's/\]//' | sed 's/,/+/g')
+
+  # If workspace_seconds is blank, set to 0.
+  if [ ! "$workspace_seconds" ]
+  then
+   workspace_seconds=0
+  fi 
+
+   # Add workspace_seconds to total_seconds.
    total_seconds=$(($total_seconds+$workspace_seconds))
 done
 
