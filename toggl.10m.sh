cd9d31946fd803eff3f8b57998d8e9a3ed2b59a9
Rob Dyson <rdyson@hotmail.com>
2022-10-30 14:39:43 +0000

    Move workspaces to xbar variable, add option for selecting which day starts the week

--- toggl.10m.sh
+++ toggl.10m.sh
@@ -1,24 +1,24 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
 export PATH='/bin:/usr/local/bin:/usr/bin:/opt/homebrew/bin:$PATH'
+
 #  <xbar.title>Toggl Xbar</xbar.title>
 #  <xbar.version>v1.0</xbar.version>
 #  <xbar.author>Rob Dyson</xbar.author>
 #  <xbar.author.github>rdyson</xbar.author.github>
 #  <xbar.desc>Displays current week's total time logged in Toggl for one or more workspaces</xbar.desc>
+#  <xbar.var>string(VAR_WORKSPACES): Workspace ID(s). Separate multiple IDs with a space.</xbar.var>
+#  <xbar.var>string(VAR_TOGGL_API_KEY): Toggl API Key.</xbar.var>
+#  <xbar.var>select(VAR_WEEK_START_DAY="Monday"): Week starts on [Monday, Sunday]</xbar.var>
 
-# Separate multiple workspaces with a space
-workspaces=(2838545 6790797)
-
-# Specifies previous Monday, can be changed to Sunday if you prefer
-start_date=$(date -v -Monday +"%Y"-"%m"-"%d")
+# Specifies previous Monday, can be changed to Sunday if you prefer.
+start_date=$(date -v -${VAR_WEEK_START_DAY} +"%Y"-"%m"-"%d")
 
 # Loop over all workspaces in workspaces array, summing total seconds.
-# Curl uses login credentials stored in ~/.netrc, see repo for an example.
-# Get the "tracked_seconds" value from the curl response and remov brackets and padding.
-
-for workspace in ${workspaces[@]}
+# Get the "tracked_seconds" value from the curl response and remove brackets and padding.
+for workspace in ${VAR_WORKSPACES[@]}
 do
- workspace_seconds=$(curl -s -X POST https://api.track.toggl.com/reports/api/v3/workspace/"$workspace"/projects/summary \
+  workspace_seconds=$(curl -s -X POST https://api.track.toggl.com/reports/api/v3/workspace/"$workspace"/projects/summary \
   -H "Content-Type: application/json" \
   -d '{"start_date":"'"$start_date"'"}' \
   -n | jq -c '[.[].tracked_seconds'] | sed 's/\[//' | sed 's/\]//' | sed 's/,/+/g')
@@ -28,15 +28,23 @@ do
   then
    workspace_seconds=0
   fi 
-
-   # Add workspace_seconds to total_seconds.
-   total_seconds=$(($total_seconds+$workspace_seconds))
+ 
+  # Add workspace_seconds to total_seconds.
+  total_seconds=$(($total_seconds+$workspace_seconds))
 done
 
-# Print total seconds as hours.
+# Get total seconds as hours, or 0 if no time found.
 if [[ $total_seconds == 0 ]]
 then
- echo "0h";
+  output="0h";
 else
- awk  'BEGIN { rounded = sprintf("%.0f", "'"$total_seconds"'"/3600); hours = "h"; result = rounded hours; print result }'
+  output=$(awk 'BEGIN { rounded = sprintf("%.0f", "'"$total_seconds"'"/3600); hours = "h"; result = rounded hours; print result }')
 fi
+
+# Print final result
+echo $output
+
+# Add menu item to go to Toggl website
+echo "---"
+echo "Toggl Website | href=http://track.toggl.com"
+
